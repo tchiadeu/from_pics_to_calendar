1c2256616ff6c8e621a7e21fec3842386fd5b106
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-09-21 19:52:14 +0200

    method upload

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,16 @@ class PagesController < ApplicationController
   def home; end
 
   def new_file; end
+
+  def upload
+    if params[:image].present?
+      file_path = Rails.root.join('app', 'assets', 'images', 'upload', params[:image].original_filename)
+      File.open(file_path, 'wb') do |file|
+        file.write(params[:image].read)
+      end
+      redirect_to new_event_path(file_name: params[:image].original_filename)
+    else
+      render :new_file, status: :unprocessable_entity
+    end
+  end
 end
