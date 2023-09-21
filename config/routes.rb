26a67c62089a1a7b7b94c932a91d19b79d44ccff
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-09-21 15:59:47 +0200

    create controllers for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  devise_for :users
-
+  root 'pages#home'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
 end
