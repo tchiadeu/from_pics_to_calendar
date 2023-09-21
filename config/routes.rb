e8b4dfd0f95e28e1c83a4fe44b58af3b5c4b82a8
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-09-21 19:50:03 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
+  get 'new_file', to: 'pages#new_file'
+  post 'upload', to: 'pages#upload'
+  get 'new_event', to: 'pages#new_event'
+  post 'create_event', to: 'pages#create_event'
 end
