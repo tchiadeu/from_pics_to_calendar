1f75dfdae20db68bb9aae6a5854b3f79fbdb91c2
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-09-23 15:32:50 +0200

    add redirection to presentation

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    user = User.from_omniauth(auth)
-
-    if user.present?
-      sign_out_all_scopes
-      sign_in_and_redirect user, event: :authentication
-    elsif user.unauthorized?
+    if auth.info.email == 'contact@kevintchiadeu.com'
+      user = User.from_omniauth(auth)
+      if user.present?
+        sign_out_all_scopes
+        sign_in_and_redirect user, event: :authentication
+      end
+    else
       redirect_to new_user_registration_path
     end
   end
