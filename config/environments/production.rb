20004af25930f3f3d54325f1da502f6d2f58d997
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-09-25 22:48:03 +0200

    modify db for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.active_record.sqlite3_production_warning = false
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
