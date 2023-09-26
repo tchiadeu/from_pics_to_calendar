ad86dc5f8695eaa92eba473f52c69a8ee89b4e33
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-09-26 20:21:48 +0200

    modify to 1 to 1 relation type

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
-  has_many :photos
+  has_one :photo, dependent: :destroy
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
