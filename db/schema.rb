91879f0b7b22ca6fc66981a2f04977f368df32bf
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-09-26 20:19:59 +0200

    add foreign key user to photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_26_180946) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_26_181934) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -42,6 +42,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_26_180946) do
   create_table "photos", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,4 +62,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_26_180946) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "photos", "users"
 end
