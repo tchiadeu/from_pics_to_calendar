396238a36c944348fd73872c28e78429b988fbe5
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-09-26 22:58:23 +0200

    try something

--- config/initializers/google_cloud_vision.rb
+++ config/initializers/google_cloud_vision.rb
@@ -1,5 +1,9 @@
 require "google/cloud/vision"
 
 Google::Cloud::Vision.configure do |config|
-  config.credentials = ENV["GOOGLE_CLOUD_API_KEY"]
+  if Rails.env.production?
+    config.credentials = JSON.parse(ENV['GOOGLE_CLOUD_API_KEY'])
+  else
+    config.credentials = ENV['GOOGLE_CLOUD_API_KEY']
+  end
 end
