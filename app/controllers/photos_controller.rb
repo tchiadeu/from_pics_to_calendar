b300fd32255f9faa75190c7cce211be3dd26de39
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-09-26 20:15:49 +0200

    add methods for photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,23 @@
 class PhotosController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    @photo.image.attach(params[:image])
+    if @photo.save
+      redirect_to new_event_path(file_name: params[:image].original_filename)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:image)
+  end
 end
